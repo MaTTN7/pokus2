8bb3090903c02c5217270afcf3da62ae5fd2f3b8
Pavel Kurka <kurka.hostavice@seznam.cz>
2016-07-23 20:05:43 +0200

    Add project files.

---
+++ CviceniL1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CviceniL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, jak se jmenuješ?");
+            string jmeno = Console.ReadLine();
+            Console.WriteLine("Jaký jsi?");
+            string vlastnost = Console.ReadLine();
+            vlastnost = vlastnost.ToLower();
+            Console.WriteLine(jmeno + " je " + vlastnost + ".");
+            Console.ReadKey();
+        }
+    }
+}
